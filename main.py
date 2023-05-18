098f41e6c2db021454ee72d7ee5f3d2b4be08e0f
Obiora Okafor <obioracelestine@gmail.com>
2023-05-18 15:30:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -56,10 +56,7 @@ Base = declarative_base()
 login_manager = LoginManager()
 login_manager.session_protection = "strong"
 login_manager.login_view = 'login'
-login_manager.refresh_view = 'relogin'
-login_manager.needs_refresh_message = u"User session expired, please re-login"
 login_manager.login_message_category = "info"
-login_manager.needs_refresh_message_category = "info"
 login_manager.init_app(app)
 
 
